9036b622a8a549f36d8a82b80632909893328a1b
lwr.dgas <lwr_dgas@163.com>
2019-07-24 11:56:15 +0800

    修改默认配置文件

--- tools/config.py
+++ tools/config.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
 
 import os
-
 
 title = "纳兰秋水"
 
@@ -14,23 +12,22 @@ ansible_result_redis_db = 10
 
 
 #### Redis  ~~~ ansible 结果临时存放+Celery
-REDIS_ADDR = "10.20.88.215"
-REDIS_PORT = 6479
-REDIS_PD = '310c8cabcdefghf2d8abcdefd44496ac80'
+REDIS_ADDR = "127.0.0.1"
+REDIS_PORT = 6379
+REDIS_PD = ''
 
 #### Celery
-BROKER = "redis://:%s@10.20.88.215:6479/3" % REDIS_PD
-BACKEND = "redis://:%s@10.20.88.215:6479/4" % REDIS_PD
+BROKER = "redis://:%s@127.0.0.1:6379/3" % REDIS_PD
+BACKEND = "redis://:%s@127.0.0.1:6379/4" % REDIS_PD
 
-##### MYSQL    
-USE_MYSQL = False 
-MYSQL_HOST = '10.20.88.215'
-MYSQL_PORT = 23309
-MYSQL_USER = 'root'
-MYSQL_PASS = 'newpass'
-
-
-######## Note
-note_base_dirt = 'notes'
+##### MYSQL   
+USE_MYSQL = False  # False; 忽略
+#MYSQL_HOST = '127.0.0.1'
+#MYSQL_PORT = 3306
+#MYSQL_USER = 'root'
+#MYSQL_PASS = 'newpass'
 
 web_debug = True
+
+note_base_dirt = 'notes'
+inventory = 'inventory'
