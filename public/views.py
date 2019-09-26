6095b3d03aa8863ea04fe012bee2847c56128a63
独孤傲 <lwr_dgas@163.com>
2019-09-27 00:13:34 +0800

    新前端视图

--- public/views.py
+++ public/views.py
@@ -1,15 +1,67 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
+from django.http import HttpResponse, JsonResponse
+
 import json, random, datetime
 
-
 from public.models import *
+
 class Index(View):
     def get(self, request, *k, **kw):
         data = {}
-        t = datetime.date.today()
-        tmp = [  (t+datetime.timedelta(i)) for i in range(-6, 1) ]
-        data['x'] = [  i.strftime('%m/%d') for i in tmp ]
-        data['y'] = [AnsibleTasks.objects.filter(AnsibleID__contains=i.strftime('%Y%m%d')).count() for i in tmp]
+        # t = datetime.date.today()
+        # tmp = [  (t+datetime.timedelta(i)) for i in range(-6, 1) ]
+        # data['x'] = [  i.strftime('%m/%d') for i in tmp ]
+        # data['y'] = [AnsibleTasks.objects.filter(AnsibleID__contains=i.strftime('%Y%m%d')).count() for i in tmp]
         # data['y'] = [ random.randint(2,28) for i in  range(7) ]
-        return render(request, 'base_public.html', data)
+        # return render(request, 'base_public.html', data)
+        return render(request, 'base_adminlte.html', data)
+
+
+class AnsibleTaskList(ListView):
+    model = AnsibleTasks
+
+class AnsibleTaskDetail(DetailView):
+    model = AnsibleTasks
+
+    def get(self, request, *k, **kw):
+        if request.is_ajax():
+            return JsonResponse({"msg": "ok"})
+        else:
+            return super().get(request, *k, **kw)
+
+class GroupsDetail(DetailView):
+    model = ProjectGroups
+
+class GroupsList(ListView):
+    model = ProjectGroups
+
+class HostsList(ListView):
+    model = HostsLists
+
+class HostsDetail(DetailView):
+    model = HostsLists
+
+class PlaybookList(ListView):
+    model = Functions
+
+class PlaybookDetail(DetailView):
+    model = Functions
+
+class TemplateReturn(LoginRequiredMixin, View):
+    login_url = '/account/login'
+    redirect_field_name = 'redirect_to'
+
+    def get(self, request, *args, **kw):
+        print(args, kw)
+        template_file = kw.get('template_file', '')
+
+        groups = ProjectGroups.objects.all()
+        functions = Functions.objects.all()
+
+        return render(request, template_file, {'groups': groups, 'functions': functions})
+
+    def post(self, request, *k, **kw):
+        return JsonResponse({'data': {'k': k, 'kw': kw}})
