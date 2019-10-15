bca67e7de48b32cabcae572965728e8299e1e2e2
lwr.dgas <lwr_dgas@163.com>
2019-10-15 15:16:45 +0800

    passws

--- insert_data.py
+++ insert_data.py
@@ -14,14 +14,14 @@ from django.contrib.auth.models import User
 from tools.config import inventory
 
 
-u,b = User.objects.get_or_create(username='root') 
+u,b = User.objects.get_or_create(username='root')
 if b:
     u.username = 'root'
     u.is_staff = True
     u.is_superuser = True
     u.set_password('1234567')
     u.save()
-    print("\33[34m创建管理员，root: 12334567")
+    print("\33[34m创建管理员，root: 1234567")
 else:
     print('超级管理员已存在')
 
@@ -35,11 +35,18 @@ if b:
 else:
     print('测试 playbook 已存在')
 
+pb,b = Functions.objects.get_or_create(playbook='wordpress.yml')
+if b:
+    print('创建部署wordpress playbook')
+    pb.nickName = '部署wordpress'
+    pb.playbook = 'wordpress.yml'
+    pb.save()
+else:
+    print('测试部署wordpress 已存在')
 
-h,b = HostsLists.objects.get_or_create(hostName='localhost', hostAddr='127.0.0.1')
+h,b = HostsLists.objects.get_or_create(hostName='测试主机', hostAddr='1.1.1.1')
 if b:
     print('添加本机')
-
 
 g,b = ProjectGroups.objects.get_or_create(groupName='test',nickName='测试')
 if b:
@@ -50,7 +57,7 @@ g.save()
 
 data = "# 请勿手动修改该文件\n"
 gs = ProjectGroups.objects.all()
-for g in gs: 
+for g in gs:
     data += '\n# %s\n[%s]\n' % (g.nickName, g.groupName)
     data += '\n'.join([ i[0] for i in g.hostList.values_list('hostAddr') ])
 with open(inventory, 'w') as f:
